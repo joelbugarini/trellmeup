cd72ceb2cd1a1280468f29fe5a5953df9e20338d
Joel Bugarini <joelbugarini@gmail.com>
2020-12-16 02:18:06 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using ExcelDataReader;
+using System;
+using System.IO;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace trellmeup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("trello analysis generator");
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            var filePath = "Observaciones.xlsx";
+            int ct = 0; 
+
+            var tickets = new List<Ticket>();
+            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {                    
+                    do
+                    {
+                        while (reader.Read())
+                        {
+                           try{
+                                var ticket = new Ticket();
+                                ticket.Populate(reader, ct++);
+                                tickets.Add(ticket);
+                           } catch{}
+                                                        
+                        }
+                    } while (reader.NextResult());
+                }
+            }
+
+           foreach(Ticket ticket in tickets
+                   .Where(x => !x.Title.Contains("[archived]"))
+                   .Where(x => x.List == "Backlog" || 
+                               x.List == "Sprint Backlog" ||
+                               x.List == "In Progress" ||
+                               x.List == "Complete" ||
+                               x.List == "Code Review")
+                   .OrderBy(x => x.List == "Complete"? 1 :
+                                 x.List == "Code Review"? 2 :
+                                 x.List == "In Progress"? 3 :
+                                 x.List == "Sprint Backlog"? 4 :
+                                 x.List == "Backlog"? 5 : 6)
+                   .ThenBy(x => x.Id)) 
+           {
+               Console.WriteLine(ticket.Id + " " + ticket.List + " " +ticket.CardNo + "  " + ticket.Title);
+           }
+        }
+    }
+}
---
+++ Ticket.cs
@@ -0,0 +1,54 @@
+using System;
+using ExcelDataReader;
+
+namespace trellmeup
+{
+    public class Ticket
+    {
+       public int Id { get; set; }
+       public string List { get; set; } 
+       public string Title { get; set; }
+       public string Description { get; set; }
+       public int Points { get; set; }
+       public string Due { get; set; }
+       public string Members { get; set; }
+       public string Labels { get; set; }
+       public int CardNo { get; set; }
+       public string CardURL { get; set; }
+
+       public void Populate(IExcelDataReader reader, int _Id)
+       {
+           Id = _Id;
+       
+           List = reader.GetString(0);
+           Title = reader.GetString(1);
+           Description = reader.GetString(2);
+           Points = Integerize(reader, 3);
+           Due = reader.GetString(4);
+           Members = reader.GetString(5);
+           Labels = reader.GetString(6);
+           CardNo = Integerize(reader, 7);
+           CardURL = reader.GetString(8);
+       }
+
+        private int Integerize(IExcelDataReader reader, int position)
+        {
+            int result = 0;
+            var fileType = reader.GetFieldType(position);
+            if(fileType.Equals(typeof(System.Int32)))
+            {
+                return reader.GetInt32(position);
+            }
+            if(fileType.Equals(typeof(System.Double)))
+                return Convert.ToInt32(reader.GetDouble(position));
+            if(fileType.Equals(typeof(System.String)))
+            {
+                int.TryParse(reader.GetString(position), out result);
+                return result;
+            }
+
+            return 0;
+
+        }
+    }
+}
